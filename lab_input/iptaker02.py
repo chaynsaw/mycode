6ba5df59496ae41267553a10514689c82202e923
Chaynor Hsiao <chaynsaw@gmail.com>
2021-05-10 19:28:10 +0000

    input statements

---
+++ lab_input/iptaker02.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+user_input = input("Please enter an IPv4 IP address: ")
+vendor_name = input("Please enter a vendor name: ")
+ 
+# print("You told me the IPv4 address is:" + user_input)
+
+## print() can be given a series of objects separated by a comma
+print("You told me the IPv4 address is:", user_input)
+print("You told me the vendor name is:", vendor_name)
+##
+
