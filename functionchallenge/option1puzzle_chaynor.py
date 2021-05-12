4be0634e2b18a9397d4b8d788dafc9348d85ef8b
Chaynor Hsiao <chaynsaw@gmail.com>
2021-05-12 14:52:17 -0700

    Make into functions

--- functionchallenge/option1puzzle_chaynor.py
+++ functionchallenge/option1puzzle_chaynor.py
@@ -1,15 +1,20 @@
-with open("puzzle1.txt", "r") as numfile:
-    nums = []
-    for num in numfile.readlines():
-        nums.append(int(num))
+def find_two_sum_and_multiply(filename="puzzle1.txt"):
+    with open(filename, "r") as numfile:
+        nums = []
+        for num in numfile.readlines():
+            nums.append(int(num))
+        for num in nums:
+            complement = 2020 - num
+            if complement in nums:
+                print(num * complement)
+                print("The two numbers are", num, complement)
+                break
 
-    for num in nums:
-        complement = 2020 - num
-        if complement in nums:
-            print(num * complement)
-            print("The two numbers are", num, complement)
-            break
-
+def find_three_sum_and_multiply(filename="puzzle1.txt"):
+    with open(filename, "r") as numfile:
+        nums = []
+        for num in numfile.readlines():
+            nums.append(int(num))
     for i in range(len(nums)):
         for j in range(i, len(nums)):
             for k in range(j, len(nums)):
@@ -17,3 +22,6 @@ with open("puzzle1.txt", "r") as numfile:
                     print(nums[i] * nums[j] * nums[k])
                     print("The three numbers are", nums[i], nums[j], nums[k])
 
+if __name__ == "__main__":
+    find_two_sum_and_multiply()
+    find_three_sum_and_multiply()
