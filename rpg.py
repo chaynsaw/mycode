ff04be71a7ce86107ba6074f1412247485a2be8c
Chaynor Hsiao <chaynsaw@gmail.com>
2021-05-15 14:03:07 -0700

    Add Challenge and Monster, as well as PEP8 corrections

--- rpg.py
+++ rpg.py
@@ -7,6 +7,8 @@ ROOM_DESCRIPTIONS = [
     'A bright, warm room. It feels cozy. You could fall asleep here.',
     'A dark, warm room. No idea what is generating warmth, certainly not the torch illuminating just enough the staircase up, and the doorways leading out.'
 ]
+
+
 class Game:
     def __init__(self, name: str):
         self.player = Player(name)
@@ -38,7 +40,10 @@ class Game:
         print("In front of you, you can see a doorway. In the middle of the room, a staircase to the upper floor.\n")
         print("A clear, even voice echoes somewhere, not so distant, not so close.\n")
         print(f"'Hello {self.player.name}.'")
-        print("'Do not trouble yourself with questions of who you are, and why you are here. You are here, in this Tower, and there is no way out but through. Climb the tower. Reach the end. I will explain everything then.\n")
+        print(
+            "'Do not trouble yourself with questions of who you are, and why you are here. You are here, "
+            "in this Tower, and there is no way out but through. Climb the tower. Reach the end. I will explain "
+            "everything then.\n")
         input("Press enter to continue.")
 
     def proceed(self):
@@ -47,9 +52,45 @@ class Game:
         print(self.map.access_current_room().description)
         self.move_player()
 
+
 class Player:
     def __init__(self, name: str):
         self.name = name
+
+
+class Challenge:
+    def __init__(self):
+        self.cleared = False
+
+    def describe(self):
+        if self.cleared is False:
+            print("You must clear this challenge.")
+        else:
+            print("You have cleared this challenge.")
+
+
+class Monster(Challenge):
+    ranges = [(1, 5), (2, 5), (5, 9), (3, 10), (2, 4), (1, 2), (4, 9)]
+
+    def __init__(self):
+        super().__init__()
+        self.health = random.choice([30, 40, 50, 60])
+        self.damage = random.choice(self.ranges)
+        print(self.damage)
+
+    def fight(self, damage_range):
+        if self.health <= 0:
+            print("The monster is dead.")
+        else:
+            damage_dealt_to_monster = random.randint(*damage_range)
+            damage_dealt_to_player = random.randint(*self.damage)
+            self.health -= damage_dealt_to_monster
+            print(f"Monster takes {damage_dealt_to_monster} dmg and deals {damage_dealt_to_player} dmg!")
+            if self.health <= 0:
+                print("Monster has died from its wounds.")
+            else:
+                print(f"Monster has {self.health} HP left.")
+
 
 class Room:
     def __init__(self):
@@ -64,6 +105,7 @@ class Room:
         if self.player_is_here is True:
             result = "[ x ]"
         return result
+
 
 class Map:
     def __init__(self, height=5, length=5):
@@ -80,7 +122,8 @@ class Map:
 
     def move_player(self, proposed_position):
         try:
-            if 0 > proposed_position[0] or proposed_position[0] >= self.height or 0 > proposed_position[1] or proposed_position[1] >= self.length:
+            if 0 > proposed_position[0] or proposed_position[0] >= self.height or 0 > proposed_position[1] or \
+                    proposed_position[1] >= self.length:
                 raise ValueError("Move out of bounds")
 
             self.layout[proposed_position[0]][proposed_position[1]].player_is_here = True
@@ -99,6 +142,7 @@ class Map:
                 print(self.layout[x][y].show_on_map(), end=" ")
             print("")
 
+
 def main():
     # input_name = input("What is your name? Press Enter to select the default name.")
     # player_name = input_name if input_name is not "" else "Chadwick Feeserton"
@@ -107,7 +151,6 @@ def main():
     game.begin()
     game.proceed()
     game.move_player()
-
 
 
 main()
